8cce811ae9162d40745d5b03181067e12b5e47b0
Allah Bakhsh <malikallahbuhksh@gmail.com>
2024-08-06 10:11:14 -0700

    solve the week2 assignment1

---
+++ assignment_solution_1.py
@@ -0,0 +1,97 @@
+simple_message:str = "This is my simple message in python."
+print(simple_message)
+
+message:str = "Python is simple and easy to understand."
+print(message)
+
+message = "How are you my friend?"
+print(message)
+
+person_name:str = "Muhammad Tariq"
+print(f"Hello {person_name}, would yo like to learn some python today?")
+
+name = "Muhammad tariq"
+print(f"in lowercase {name.lower()},\n now in upper case {name.upper()},\n and in title case is {name.title()}")
+
+print('''Albert Einstein once said,
+       “A person who never made a mistake never tried anything new.''')
+
+famous_person:str = "Albert Einstein"
+message:str = '"A person who never made a mistake never tried anything new."'
+
+print(f'{famous_person} once said, {message}')
+
+name:str = "\t Allah \tBakhsh\n "
+print(name)
+
+print(name.lstrip())
+
+print(name.rstrip())
+
+print(name.strip())
+#Task: Assign the values 5, 10, and 15 to three variables x, y, and z. Print their sum.
+
+x:int = 5
+y:int = 10
+z:int = 15
+print(f"sum of {x}, {y} and {z} is {x + y + z}")
+
+#Task: Swap the values of two variables a and b. Print the values before and after the swap.
+a: int = 20
+b: int = 30
+print(f'Before swap: a = {a}, b = {b}')
+c = a
+a = b
+b = c
+print(f'After swap: a = {a}, b = {b}')
+
+favorite_color:str = "Red"
+print(favorite_color)
+color_favorite:str =  favorite_color
+
+print(color_favorite)
+
+pet_name:str = "Buddy"
+pet_name = "Max"
+
+Sunshine: str = "Sunshine"
+
+print(sunshine)
+
+score: int = 100
+print(score)
+
+score = 90
+print(score)
+
+city: str = "Lahore"
+print(city)
+
+text: str = "python programming"
+print(text.title())
+
+mixed_case1:str = "Apple"
+print(mixed_case1.lower())
+
+mixed_case2:str = "Banana"
+print(mixed_case2.upper())
+
+temperature: int = 25
+
+print(f"The current temperature is {temperature} degrees.")
+
+poem:str = """
+The sun sets slow and paints the sky,
+A fiery hue that makes me sigh.
+The stars come out and twinkle bright,
+A night of rest, a peaceful sight.
+"""
+
+print(poem)
+
+print(f"The length of the poem is {len(poem)} characters.")
+
+print(f"The poem starts with: {poem[:10]}")
+
+
+
